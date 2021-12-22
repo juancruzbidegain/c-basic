d3b3b90b5487851d8a7613a5240c61b674fcf3e7
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-22 00:45:44 -0300

    Const and Prepocessor directives

--- hello.c
+++ hello.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
+#define valorDePorcion 1.5
+
 int a,b,c;
 float f,g,h;
 
+const char NEWLINE = '\n';
 
 int main()
 {	
-	
+	float costoPizza = valorDePorcion * 6;
+
 	a = 5;
 	b = 30;
 	c = a + b;
 
 	printf("Value of sum is: %d \n", c);
+	printf("Total bill: %f \n", costoPizza);
+	printf("%c", NEWLINE);
+	printf("*** Final del programa *** %c", NEWLINE);
+
 	return 0;	
 	
 }
