bb4c6cbf9e39dfd3e8d352d2761bb4591bc33685
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-21 23:59:32 -0300

    Data types

--- hello.c
+++ hello.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+enum weekDays {
+	sunday,
+	monday,
+	tuesday,
+	wednesday,
+	thursday,
+	friday,
+	saturday
+};
+
 int main()
 {
-	printf("Hello platzi \n");
+	
+	enum weekDays today;
+	today = sunday;
+
+	printf("Day %d \n", today + 1);
 	return 0;	
+	
 }
