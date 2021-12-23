d490336c7b14f1e7b3d68d244981932256331b9d
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-22 22:21:31 -0300

    Add potencia

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 /*
 
+Estrucutura de una funcion en C
+
 return-type function-name(parameter declartions, if any){
     declarations;
     statemets;
@@ -10,8 +12,29 @@ return-type function-name(parameter declartions, if any){
 
 */
 
+//Init function
+int power(int base, int n);
+
 int main()
 {	
-	
-	
-}
+	int i;
+	for(i = 0; i < 10; i++)
+	{
+		printf("| n = %d resultado de la potencia = %d | \n", i, power(2,i));
+	};
+	return 0;
+};
+
+
+int power(int base, int n)
+{
+	int i, p;
+	p = 1;
+
+	for(i = 1 ; i <= n ; i++)
+	{
+		p = p * base;
+	};
+
+	return p;
+};
